9c7a07edd377c6a983b7d34720d4179f9ce712e1
isha3011 <84734703+isha3011@users.noreply.github.com>
2023-08-26 23:04:10 +0530

    Add files via upload

---
+++ javascript/watch.js
@@ -0,0 +1,35 @@
+let [seconds, minutes, hours]=[0,0,0];
+let displayTime=document.getElementById("display");
+let timer=null;
+
+function stopwatch(){
+    seconds++;
+    if(seconds==60){
+        seconds=0;
+        minutes++;
+        if(minutes==60){
+            minutes=0;
+            hours++;
+        }
+    }
+    let hr= hours<10 ? "0" + hours : hours;
+    let min= minutes<10 ? "0" + minutes : minutes;
+    let sec= seconds<10 ? "0" + seconds : seconds;
+    displayTime.innerHTML= hr + ":" + min + ":" + sec ; 
+}
+
+function watchstart(){
+    if(timer!=null){
+        clearInterval(timer);
+    }
+    timer=setInterval(stopwatch, 1000);
+}
+function watchstop(){
+    clearInterval(timer);
+    
+}
+function watchrestart(){
+    clearInterval(timer);
+    [seconds, minutes, hours]=[0,0,0];
+    display.innerHTML="00:00:00";
+}
